f0eddeb36ee823cb2654656a9584b0adc5156dbe
keep_wan <whevether@outlook.com>
2022-12-20 10:48:09 +0800

    第一次提交

---
+++ Generator/Code/ICaptchaCodeGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wjsz.Captcha.Generator.Code
+{
+    public interface ICaptchaCodeGenerator
+    {
+        (string renderText, string code) Generate(int length);
+    }
+}
