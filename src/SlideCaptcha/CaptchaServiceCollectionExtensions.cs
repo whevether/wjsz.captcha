26a068d4098194f039a037eb72baeb0a98a67378
whevether <whevether@outlook.com>
2023-12-05 11:22:32 +0800

    update

---
+++ src/SlideCaptcha/CaptchaServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using SlideCaptcha.Core;
+using SlideCaptcha.Core.Generator;
+using SlideCaptcha.Core.Resources;
+using SlideCaptcha.Core.Resources.Handler;
+using SlideCaptcha.Core.Resources.Provider;
+using SlideCaptcha.Core.Storage;
+using SlideCaptcha.Core.Validator;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class CaptchaServiceCollectionExtensions
+    {
+        public static CaptchaBuilder AddSlideCaptcha(this IServiceCollection services, IConfiguration configuration, Action<CaptchaOptions> optionsAction = default)
+        {
+            services.Configure<CaptchaOptions>(configuration?.GetSection("SlideCaptcha"));
+            if (optionsAction != null) services.PostConfigure(optionsAction);
+
+            var builder = new CaptchaBuilder(services);
+            services.AddSingleton<IResourceProvider, OptionsResourceProvider>();
+            services.AddSingleton<IResourceProvider, EmbeddedResourceProvider>();
+            services.AddSingleton<IResourceHandlerManager, CachedResourceHandlerManager>();
+            services.AddSingleton<IResourceManager, DefaultResourceManager>();
+            services.AddSingleton<IResourceHandler, FileResourceHandler>();
+            services.AddSingleton<IResourceHandler, EmbeddedResourceHandler>();
+            services.AddScoped<ICaptchaImageGenerator, DefaultCaptchaImageGenerator>();
+            services.AddScoped<ICaptcha, DefaultCaptcha>();
+            services.AddScoped<IStorage, DefaultStorage>();
+            services.AddScoped<IValidator, SimpleValidator>();
+            return builder;
+        }
+    }
+}
---
+++ src/SlideCaptcha/Resources/Provider/IResourceProvider.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlideCaptcha.Core.Resources.Provider
+{
+    public interface IResourceProvider
+    {
+        List<Resource> Backgrounds();
+        List<TemplatePair> Templates();
+    }
+}
